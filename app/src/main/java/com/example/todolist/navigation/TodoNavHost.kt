4eae062364f3dd4cb55e157baf1d075f6577e949
Mateus Prado <maateussp@gmail.com>
2025-04-17 13:27:57 -0300

    Navegações e nota tela

---
+++ app/src/main/java/com/example/todolist/navigation/TodoNavHost.kt
@@ -0,0 +1,32 @@
+package com.example.todolist.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.todolist.ui.feature.AddEditScreen
+import com.example.todolist.ui.feature.ListScreen
+import kotlinx.serialization.Serializable
+import androidx.navigation.toRoute
+
+
+@Serializable
+object ListRoute
+
+@Serializable
+data class AddEditRoute (val id: Long? = null)
+
+@Composable
+fun  TodoNavHost(){
+    val navController = rememberNavController()
+    NavHost(navController =  navController, startDestination = ListRoute) {
+        composable<ListRoute> {
+            ListScreen()
+        }
+        composable<AddEditRoute> { backStackEntry ->
+            val addEditRoute = backStackEntry.toRoute<AddEditRoute>()
+            AddEditScreen()
+        }
+    }
+}
